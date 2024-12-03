3a1a3403ed3d3499cbe820a256ea365034c61e4a
Ruwini Tharanga <128955270+ruwini01@users.noreply.github.com>
2024-12-03 17:19:18 +0000

    Added all cards and Cart page

--- myapp/src/components/Products.js
+++ myapp/src/components/Products.js
@@ -1,8 +1,19 @@
-
+import {useState} from 'react'
 import '../assets/CSS/layout.css';
+import Cart from './Cart';
+import {flowers} from './FlowerDB';
+import Product from './Product';
 
 
 export default function Products(){
+
+    const [items, setItems] = useState([]);
+    const [grandTotal, setGrandTotal] = useState(0);
+
+    const addToCart = (item)=>{
+        setItems([...items, item]);
+        setGrandTotal((prev)=> prev+item.price);
+    }    
    
     return(
         <>
@@ -13,14 +24,14 @@ export default function Products(){
                 <h4 className="card-title">Buy flowers</h4>
                 <div className="grid-container">
                     {
-                        //product
+                        flowers.map((flower)=>(<Product data={flower} cartFunc={addToCart}/>))
                     }
                 </div>
 
             </div>
             <div className="item3">
                 {
-                //cart
+                    <Cart items={items} total={grandTotal}/>
                 }
             </div>
         </>
